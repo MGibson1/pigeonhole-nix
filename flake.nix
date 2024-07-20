1dc69ce9029985f3694372fd3a6b7baa71dcbb33
Matt Gibson <git@mgibson.dev>
2024-07-19 23:02:42 -0700

    Swap to fenix for rust packages

--- flake.nix
+++ flake.nix
@@ -1,51 +1,66 @@
 {
-  description = "A Nix-flake-based Rust development environment";
-
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, rust-overlay }:
-    let
-      overlays = [
-        rust-overlay.overlays.default
-        (final: prev: {
-          rustToolchain =
-            let
-              rust = prev.rust-bin;
-            in
-            if builtins.pathExists ./rust-toolchain.toml then
-              rust.fromRustupToolchainFile ./rust-toolchain.toml
-            else if builtins.pathExists ./rust-toolchain then
-              rust.fromRustupToolchainFile ./rust-toolchain
-            else
-              rust.stable.latest.default.override {
-                extensions = [ "rust-src" "rustfmt" ];
-              };
-        })
-      ];
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit overlays system; };
-      });
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            rustToolchain
-            openssl
+  outputs = {
+    self,
+    nixpkgs,
+    fenix,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            fenix.overlays.default
+          ];
+        };
+        nativeBuildInputs = with pkgs;
+          [
+            (fenix.packages."${system}".stable.withComponents [
+              "cargo"
+              "rust-src"
+              "rust-docs"
+              "rustc"
+            ])
+            rust-analyzer
             pkg-config
+            openssl
+            clang
+            cmake
             cargo-deny
             cargo-edit
             cargo-watch
-            rust-analyzer
+            alejandra
+          ]
+          ++ lib.optionals stdenv.isDarwin [
+            pkgs.darwin.apple_sdk.frameworks.Security
+            pkgs.darwin.apple_sdk.frameworks.CoreFoundation
+          ];
+      in {
+        devShells.default = pkgs.mkShell {
+          inherit nativeBuildInputs;
+          RUST_SRC_PATH = "${fenix.packages.${system}.stable.rust-src}/bin/rust-lib/src";
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath nativeBuildInputs;
+          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+          NIX_LDFLAGS = "${pkgs.lib.optionalString pkgs.stdenv.isDarwin "\
+            -F${pkgs.darwin.apple_sdk.frameworks.Security}/Library/Frameworks -framework Security \
+            -F${pkgs.darwin.apple_sdk.frameworks.CoreFoundation}/Library/Frameworks -framework CoreFoundation"}";
+          buildInputs = with pkgs; [
+            (fenix.packages."${system}".stable.withComponents ["clippy" "rustfmt"])
+            # setting LIBCLANG_PATH manually breaks globally installed `ssh` binary and transitively breaks git
+            # so we use a local version of `ssh` in this dev env (maybe there is a better way to fix it)
+            openssh
           ];
         };
-      });
-    };
+      }
+    );
 }
